bdbfcfc2e52dfec2e112eb8ce0792e6cf3eb2c46
iodar <granzowdario@gmail.com>
2020-06-04 21:34:28 +0200

    feature: reworked commands, renamed vars

--- print-packages.sh
+++ print-packages.sh
@@ -2,26 +2,26 @@
 
 SEC_UPDATES_COUNT=0
 KERNEL_UPDATES_COUNT=0
-KERNEL_UPDATES_NAMES=
+KERNEL_UPDATE_NAMES=
 SEC_UPDATE_NAMES=
 
 get-sec-and-kernel-update-count() {
-  KERNEL_UPDATES_COUNT=$(echo -n $KERNEL_UPDATES_NAMES | wc -l)
-  SEC_UPDATES_COUNT=$(echo -n $SEC_UPDATE_NAMES | wc -l)
+  KERNEL_UPDATES_COUNT=$(mintupdate-cli list -k | wc -l)
+  SEC_UPDATES_COUNT=$(mintupdate-cli list -s | wc -l)
 }
 
 print-sec-updates-message() {
-  printf "%s security updates (%s) %s\n" "--" $SEC_UPDATES_COUNT "--"
-  printf "%s\n" "$SEC_UPDATES_NAMES"
+  printf "\n%s security updates (%s) %s\n" "--" $SEC_UPDATES_COUNT "--"
+  printf "%s\n\n" "$SEC_UPDATE_NAMES"
 }
 
 print-kernel-updates-message() {
   printf "%s kernel updates (%s) %s\n" "--" $KERNEL_UPDATES_COUNT "--"
-  printf "%s\n" "$KERNEL_UPDATES_NAMES"
+  printf "%s\n\n" "$KERNEL_UPDATE_NAMES"
 }
 
 get-kernel-update-names() {
-  $KERNEL_UPDATES_NAMES=$(
+  KERNEL_UPDATE_NAMES=$(
     echo $(echo -n "$(mintupdate-cli list -k |
     awk '{print $2 " [ current: "}')"; echo -n "$(uname -rv |
     grep -oP "^[\d\.\w\-\s#]+" |
@@ -29,18 +29,11 @@ get-kernel-update-names() {
     column -t
   )
 
-  $SEC_UPDATE_NAMES=$(
+  SEC_UPDATE_NAMES=$(
     mintupdate-cli list -s |
     awk '{print $2 " [ version: " $3 " ]"}' |
     column -t
   )
-
-
-  # mint update kernel list
-  # echo $(echo -n "$(mintupdate-cli list -k | awk '{print $2 " [ current: "}'    )"; echo -n "$(uname -rv | grep -oP "^[\d\.\w\-\s#]+" | sed 's/-generic #/./')"; echo -e " ]") | column -t
-
-  # mint update security list
-  # mintupdate-cli list -s | awk '{print $2 " [ version: " $3 " ]"}' | column -t
 }
 
 get-kernel-update-names
